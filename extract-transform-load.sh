50ae73a7430de55dc66f9a102a13c03e8f543a18
evdubs <evdubs@gmail.com>
2020-01-22 18:03:04 -1000

    add files to a 7zip archive

--- extract-transform-load.sh
+++ extract-transform-load.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+today=$(date "+%F")
 dir=$(dirname "$0")
+current_year=$(date "+%Y")
 
 racket ${dir}/extract.rkt
 racket ${dir}/transform-load.rkt -p "$1"
+
+7zr a /var/tmp/ust/yield-curve/${current_year}.7z /var/tmp/ust/yield-curve/${today}.xml
